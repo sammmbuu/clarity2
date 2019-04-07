67ea4b1867271deafb0d8bc8ab8184c7c2251a89
Sam Hearn <samhearn2010@gmail.com>
2019-04-07 07:52:07 +0000

    added music playing tab

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function hideLogin() {
     document.getElementById("login").style.display = "none";
     document.getElementById("welcome").style.display = "initial";
 }
+
+function spotifyPlay() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("playMusic").style.display = "initial";
+}
+
+function editTags() {
+    document.getElementById("playMusic").style.display = "none";
+    document.getElementById("welcome").style.display = "initial";
+}
